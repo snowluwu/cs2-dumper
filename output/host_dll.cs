61d7fb56a169352f740438478ea181772a163e0f
snowluwu <126056242+snowluwu@users.noreply.github.com>
2026-04-22 00:37:39 +0600

    feat(output): last version (dwGameRules problems)

--- output/host_dll.cs
+++ output/host_dll.cs
@@ -1,20 +1,18 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-04-03 09:44:37.704607400 UTC
+// 2026-04-21 18:33:05.371082600 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: host.dll
     // Class count: 2
     // Enum count: 0
     public static class HostDll {
-        // Parent: None
-        // Field count: 1
+        // Parent: xL_Y__
+        // Field count: 0
         public static class EmptyTestScript {
-            public const nint m_hTest = 0x10; // CAnimScriptParam<float32>
         }
         // Parent: None
-        // Field count: 1
+        // Field count: 0
         public static class CAnimScriptBase {
-            public const nint m_bIsValid = 0x8; // bool
         }
     }
 }
--- output/panorama_dll.cs
+++ output/panorama_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-04-03 09:44:37.704607400 UTC
+// 2026-04-21 18:33:05.371082600 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: panorama.dll
